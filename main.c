41391c5b3ffc76cd9b663d0596d9afc745686111
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2019-05-01 14:13:29 -0400

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,22 @@ void *emalloc(size_t size) {
     return ret;
 }
 
+void *erealloc(void *ptr, size_t size) {
+    void *ret = realloc(ptr, size);
+    if (ret == NULL) {
+        fputs("REALLOC ERROR", stderr);
+        exit(1);
+    }
+    return ret;
+}
+
+// Returns the origional on failure
+void *shrink(void *ptr, size_t size) {
+    void *ret = realloc(ptr, size);
+    if (ret == NULL) return ptr;
+    return ret;
+}
+
 struct linked_node {
     struct linked_node *nxt;
     struct linked_node *prv;
@@ -31,10 +47,20 @@ void linked_insert(struct linked_node *list, struct linked_node *in) {
     }
 }
 
-void interpret(FILE *in, FILE *out) {
+#define ARRAY_INIT_SIZE 16
+#define ARRAY_GROW 2
+
+struct funct {
+    uint8_t paramCnt;
+    uint8_t localCnt;
+
+int interpret(FILE *in, FILE *out) {
     struct linked_node *linkedOps;
     {
+        char *tmpData;
         size_t lastPos = 0;
+        size_t tmpMemLen;
+        size_t tmpDataLen;
         uint8_t isOnWord = 0;
         while (1) {
             char t;
@@ -42,7 +68,12 @@ void interpret(FILE *in, FILE *out) {
                 int r;
                 if (!fread(&r, 1, 1, in) {
                     if (ferror(in)) {
-                     = in[i] & 0x7F;
+                        fputs("File read error");
+                        return -1;
+                    } else {
+                        if (isOnWord) {
+                            
+                    t = r & 0x7F;
             if (isOnWord) {
                 if ((t <= 32) || (t >= 127)) {
                     isOnWord = 0;
@@ -54,9 +85,11 @@ void interpret(FILE *in, FILE *out) {
                 if ((t > 32) && (t < 127)) {
                     isOnWord = 1;
                     lastPos = i;
+                    tmpData = emalloc()
+                    tmpDataLen = 0;
                 }
             }
         }
     }
-    
+    struct linked_node *end = linkedOps->prv;
 }
